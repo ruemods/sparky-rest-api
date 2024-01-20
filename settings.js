47c049437b2a65e1859ab575700257ac89814f24
ruemods <105858358+ruemods@users.noreply.github.com>
2024-01-21 00:03:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Aswin Sparky'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Sperky:mole@cluster0.kglzhgp.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "sparkymon777@gmail.com" //email
+global.email_password = "sabushhiva" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
